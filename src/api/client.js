94042eb6b2fa27431fb91ca648e2f65a7f9bc947
Lynn Latt Yadi <lynnlattyadi2410@gmail.com>
2022-07-31 10:19:23 +0630

    modified client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://localhost:8000/api/" });
+const client = axios.create({
+  baseURL: "https://rakhita-music-app.herokuapp.com/api",
+});
 
 export default client;
